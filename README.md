0e0ef3cc3859884127df9af6a16c5c35d9d3d6be
Alexander Fasching <fasching.a91@gmail.com>
2018-05-07 09:49:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Webcam Recorder
+
+Simple program to preview and record a webcam stream.
---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='webcam-recorder',
+    version='0.1',
+    description='Simple program to preview and record a webcam stream.',
+    author='Alexander Fasching',
+    author_email='fasching.a91@gmail.com',
+    maintainer='Alexander Fasching',
+    maintainer_email='fasching.a91@gmail.com',
+    url='https://github.com/alexf91/webcam-recorder',
+    license='MIT',
+    packages=['webrec'],
+    entry_points={
+        'console_scripts': ['webrec = webrec.__main__:main']
+    },
+    install_requires=[
+        'numpy',
+        'opencv-python',
+    ],
+)
